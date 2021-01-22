3d8df04121296fecff732ba2beb4feaabbba057f
Chen Su Ting <wendy10935@hotmail.com>
2021-01-22 22:55:34 +1300

    Added Semantic UI

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
       <h1>Re-vents</h1>
+      <button className='ui icon red button'>
+        <i className='user icon'/> CSS Button 
+      </button>
+      <Button icon='user' content='React Button' color='green'></Button>
     </div>
   );
 }
