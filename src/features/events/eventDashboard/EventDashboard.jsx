d5e4c29aa346373a1715dbba6ea0577c5129cd01
Chen Su Ting <wendy10935@hotmail.com>
2021-06-08 23:32:46 +1200

    React hook for createEvent

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,22 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import { Grid } from 'semantic-ui-react';
 import EventForm from '../eventForm/EventForm';
 import EventList from './EventList';
-import {sampleData} from '../../../app/api/sampleData';
+import { sampleData } from '../../../app/api/sampleData';
 
-export default function EventDashboard({formOpen, setFormOpen}) {
-
+export default function EventDashboard({ formOpen, setFormOpen }) {
   // 1. Declare a state variable
   // 2. Declare a function that can update the state variable
   const [events, setEvents] = useState(sampleData);
-  
+
+  function handleCreateEvent(event) {
+    setEvents([...events, event]);
+  }
+
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events}/>
+        <EventList events={events} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && <EventForm setFormOpen={setFormOpen} setEvents={setEvents}/>}
+        {formOpen && (
+          <EventForm
+            setFormOpen={setFormOpen}
+            setEvents={setEvents}
+            createEvent={handleCreateEvent}
+          />
+        )}
       </Grid.Column>
     </Grid>
   );
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,7 +1,8 @@
+import cuid from 'cuid';
 import React, { useState } from 'react';
 import { Button, Form, Header, Segment } from 'semantic-ui-react';
 
-export default function EventForm({ setFormOpen, setEvents }) {
+export default function EventForm({ setFormOpen, setEvents, createEvent }) {
   const intialValues = {
     title: '',
     category: '',
@@ -14,7 +15,15 @@ export default function EventForm({ setFormOpen, setEvents }) {
   const [values, setValues] = useState(intialValues);
 
   function handleFormSubmit() {
-    console.log(values);
+    //console.log(values);
+    createEvent({
+      ...values,
+      id: cuid(),
+      hostedBy: 'Bob',
+      attendees: [],
+      hostPhotoURL: '/assets/user.png',
+    });
+    setFormOpen(false);
   }
 
   function handleInputChange(e) {
