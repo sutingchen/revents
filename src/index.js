0b7baaaee43254e016ef3703d3a7936fc9fc533f
Chen Su Ting <wendy10935@hotmail.com>
2021-06-12 23:18:36 +1200

    Initial routing configuration

--- src/index.js
+++ src/index.js
@@ -4,17 +4,23 @@ import 'semantic-ui-css/semantic.min.css';
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 
 function render() {
-  ReactDOM.render(<App />, rootElement)
+  ReactDOM.render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+    rootElement
+  );
 }
 
 if (module.hot) {
-  module.hot.accept('./app/layout/App', function(){
+  module.hot.accept('./app/layout/App', function () {
     setTimeout(render);
-  })
+  });
 }
 
 render();
