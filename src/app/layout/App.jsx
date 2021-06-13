d49d0efef6993175f02f1be08b5fbcfed679d8b4
Chen Su Ting <wendy10935@hotmail.com>
2021-06-13 15:38:14 +1200

    Changed HomePage routing to place it outside of NavBar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,37 +1,32 @@
-import React, { useState } from 'react';
-import { Route } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import React from 'react';
 import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
+import { Route } from 'react-router-dom';
+import HomePage from '../../features/home/HomePage';
 import EventDetailedPage from '../../features/events/eventDetailed/EventDetailedPage';
 import EventForm from '../../features/events/eventForm/EventForm';
-import HomePage from '../../features/home/HomePage';
-import NavBar from '../../features/nav/NavBar';
 
-function App() {
-  const [formOpen, setFormOpen] = useState(false);
-  const [selectedEvent, setSelectedEvent] = useState(null);
-
-  function handleSelectEvent(event) {
-    setSelectedEvent(event);
-    setFormOpen(true);
-  }
-
-  function handleCreateFormOpen() {
-    setSelectedEvent(null);
-    setFormOpen(true);
-  }
+export default function App() {
 
   return (
     <>
-      <NavBar setFormOpen={handleCreateFormOpen} />
-      <Container className="main">
-        <Route exact path='/' component={HomePage} />
-        <Route exact path='/events' component={EventDashboard} />
-        <Route path='/events/:id' component={EventDetailedPage} />
-        <Route path='/createEvent' component={EventForm} />      
-      </Container>
+      <Route exact path='/' component={HomePage} />
+      <Route
+        path={'/(.+)'}
+        render={() => (
+          <>
+            <NavBar />
+            <Container className='main'>
+              <Route exact path='/events' component={EventDashboard} />
+              <Route path='/events/:id' component={EventDetailedPage} />
+              <Route path={['/createEvent', '/manage/:id']} component={EventForm} />
+            </Container>
+          </>
+        )}
+      />
     </>
   );
 }
 
-export default App;
+
