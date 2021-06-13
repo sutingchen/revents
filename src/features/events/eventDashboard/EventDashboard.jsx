0ba2a90948a45231ee4458a8f879dca44f9ff26d
Chen Su Ting <wendy10935@hotmail.com>
2021-06-13 15:40:33 +1200

    Remove temporary handlers in EventDashboard

--- src/features/events/eventDashboard/EventDashboard.jsx
+++ src/features/events/eventDashboard/EventDashboard.jsx
@@ -1,47 +1,31 @@
 import React, { useState } from 'react';
 import { Grid } from 'semantic-ui-react';
-import EventForm from '../eventForm/EventForm';
 import EventList from './EventList';
 import { sampleData } from '../../../app/api/sampleData';
 
-export default function EventDashboard({ formOpen, setFormOpen, selectEvent, selectedEvent }) {
-  // 1. Declare a state variable
-  // 2. Declare a function that can update the state variable
+export default function EventDashboard() {
   const [events, setEvents] = useState(sampleData);
   
-  function handleCreateEvent(event) {
-    setEvents([...events, event]);
-  }
 
-  // 1. Get hold of the event we want to update
-  // 2. Swap the existing event for the updated event
-  // 3. Return the event to the setEvents function
-  function handleUpdateEvent(updatedEvent) {
-    setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
-    selectEvent(null);
-  }
+  // function handleCreateEvent(event) {
+  //   setEvents([...events, event]);
+  // }
+
+  // function handleUpdateEvent(updatedEvent) {
+  //     setEvents(events.map(evt => evt.id === updatedEvent.id ? updatedEvent : evt));
+  // }
 
   function handleDeleteEvent(eventId) {
-    setEvents(events.filter(evt => evt.id !== eventId));
-    selectEvent(null);
+      setEvents(events.filter(evt => evt.id !== eventId));
   }
 
   return (
     <Grid>
       <Grid.Column width={10}>
-        <EventList events={events} selectEvent={selectEvent} deleteEvent={handleDeleteEvent}/>
+        <EventList events={events} deleteEvent={handleDeleteEvent} />
       </Grid.Column>
       <Grid.Column width={6}>
-        {formOpen && (
-          <EventForm
-            setFormOpen={setFormOpen}
-            setEvents={setEvents}
-            createEvent={handleCreateEvent}
-            selectedEvent={selectedEvent}
-            updateEvent={handleUpdateEvent}
-            key={selectedEvent ? selectedEvent.id : null}
-          />
-        )}
+        <h2>Event Filters</h2>
       </Grid.Column>
     </Grid>
   );
